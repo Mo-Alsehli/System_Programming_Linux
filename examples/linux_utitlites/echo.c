6427d474509d2c82778cfb1adfdbdddce7b4b599
Mo Alsehli <mohamed.m.alsehli@gmail.com>
2025-03-25 03:24:14 +0200

    update space character between arguement @echo.c

--- examples/linux_utitlites/echo.c
+++ examples/linux_utitlites/echo.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]){
-	
-	for(int i = 1; i < argc; i++){
+int main(int argc, char *argv[])
+{
+
+	for (int i = 1; i < argc; i++)
+	{
 		int argLen = strlen(argv[i]);
 		// Print Arguemnts
-		if(write(1, argv[i], argLen) < 0){
+		if (write(1, argv[i], argLen) < 0)
+		{
 			printf("ERROR: writing faild!!\n");
 			exit(-1);
 		}
-		// print space to separate between arguments.
-		write(1, " ", 1);
+		// print space to separate between arguments, but not after last one
+		if (i < argc - 1)
+		{
+			write(1, " ", 1);
+		}
 	}
 	printf("\n");
 
